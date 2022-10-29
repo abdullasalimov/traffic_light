5dfd07e0d463a0ce319c2df6f9545fcbda026264
abdullasalimov <abdullasalimov7@gmail.com>
2022-10-29 21:22:06 +0500

    final

--- employee/admin.py
+++ employee/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
 from employee.models import Department, Employee
 
-admin.site.register(Department)
-admin.site.register(Employee)
+@admin.register(Department)
+class DepartmentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'level')
+
+
+@admin.register(Employee)
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display = ("fullname", 'job_title', 'employment_date', 'salary', 'department')
