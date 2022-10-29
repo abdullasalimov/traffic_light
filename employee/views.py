37f0a92467a083a4f730064a516afabf8b98285c
abdullasalimov <abdullasalimov7@gmail.com>
2022-10-29 20:12:21 +0500

    final

--- employee/views.py
+++ employee/views.py
@@ -4,6 +4,7 @@ from employee.models import Department, Employee
 def index(request):
     context = {}
     context['employee'] = Employee.objects.all()
+    context['departments'] = Department.objects.all()
     context['departments_first'] = Department.objects.filter(level='1')
     context['departments_second'] = Department.objects.filter(level='2')
     context['departments_third'] = Department.objects.filter(level='3')
